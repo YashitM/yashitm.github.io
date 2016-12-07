776cc57ef75a3208718d086fd1f2da900d2b116c
YashitM <yashitmaheshwary@gmail.com>
2016-12-07 14:23:21 +0400

    Button added and slight changes made in homepage and js

--- js/scrolldivs.js
+++ js/scrolldivs.js
@@ -1,22 +1,27 @@
 $(document).ready(function (){
   $("#AboutButton").click(function(){                
          $('html, body').animate({
-              scrollTop: $("#AboutSection").offset().top - 50
-        }, 700);               
+              scrollTop: $("#AboutSection").offset().top - 49
+        }, 1000);               
     });
   $("#HomeButton").click(function(){                
          $('html, body').animate({
               scrollTop: 0
-        }, 700);               
+        }, 1000);               
     });
   $("#ContactButton").click(function(){                
          $('html, body').animate({
-              scrollTop: $("#ContactSection").offset().top - 50
-        }, 700);               
+              scrollTop: $("#ContactSection").offset().top - 49
+        }, 1000);               
     });
   $("#ProjectsButton").click(function(){                
          $('html, body').animate({
-              scrollTop: $("#ProjectsSection").offset().top - 50
-        }, 700);               
+              scrollTop: $("#ProjectsSection").offset().top - 49
+        }, 1000);               
+    });
+  $("#LearnMoreButton").click(function(){                
+         $('html, body').animate({
+              scrollTop: $("#AboutSection").offset().top - 49
+        }, 1000);               
     });
 });
