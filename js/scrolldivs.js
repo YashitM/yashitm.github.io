40f7ead118d81af046438e2643675a1bec440bba
YashitM <yashitmaheshwary@gmail.com>
2016-12-07 15:58:35 +0400

    js flashing removed, added background images for projects section

--- js/scrolldivs.js
+++ js/scrolldivs.js
@@ -2,26 +2,31 @@ $(document).ready(function (){
   $("#AboutButton").click(function(){                
          $('html, body').animate({
               scrollTop: $("#AboutSection").offset().top - 49
-        }, 1000);               
+        }, 1000);   
+        return false;            
     });
   $("#HomeButton").click(function(){                
          $('html, body').animate({
               scrollTop: 0
-        }, 1000);               
+        }, 1000);      
+        return false;         
     });
   $("#ContactButton").click(function(){                
          $('html, body').animate({
               scrollTop: $("#ContactSection").offset().top - 49
-        }, 1000);               
+        }, 1000);        
+        return false;       
     });
   $("#ProjectsButton").click(function(){                
          $('html, body').animate({
               scrollTop: $("#ProjectsSection").offset().top - 49
-        }, 1000);               
+        }, 1000);   
+        return false;            
     });
   $("#LearnMoreButton").click(function(){                
          $('html, body').animate({
               scrollTop: $("#AboutSection").offset().top - 49
-        }, 1000);               
+        }, 1000);    
+        return false;           
     });
 });
